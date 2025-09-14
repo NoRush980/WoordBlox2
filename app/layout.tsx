53919d1bf93ab5c6a0518a6a5531af0769e5476c
NoRush980 <41871573+NoRush980@users.noreply.github.com>
2025-09-14 09:58:21 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 // app/layout.tsx
 import "./globals.css";
 
-export const metadata = { title: "Woordblox", description: "Leren + games (8–13)" };
+export const metadata = { title: "Woordblox", description: "Leren + games (8–13)" 
+                        colorScheme: "light",            // ← voeg deze regel toe
+                        };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
